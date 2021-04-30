04e8789529658cbbb42ff3c8d4e3be29491546a3
CH <chenghao@kejiankeji.cn>
2021-04-30 15:22:09 +0800

    update

--- src/components/Application/ApplicationVersionAdd/index.tsx
+++ src/components/Application/ApplicationVersionAdd/index.tsx
@@ -1,43 +1,473 @@
-import { Avatar, Card, Icon, Tag, Tooltip } from "ant-design-vue";
-import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+import {
+	AddApplicationParams,
+	AddApplicationUpdateParams,
+	ApplicationMode,
+} from "@/store/models/application/types";
+import { ProjectMemberMode } from "@/store/models/project/types";
+import {
+	Form,
+	Input,
+	Icon,
+	Button,
+	DatePicker,
+	InputNumber,
+	Row,
+	Col,
+	Select,
+	Checkbox,
+	Radio,
+	Switch,
+} from "ant-design-vue";
+import moment from "moment";
+moment.locale("zh-cn");
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+const ApplicationStore = namespace("application");
 import style from "./index.less";
-import { ApplicationMode } from "@/store/models/application/types";
 
-@Component
-export default class ApplicationVersionAdd extends Vue {
-	@Prop(Object) readonly item!: ApplicationMode;
-	render() {
+@Component({
+	props: {
+		form: {
+			type: Object,
+		},
+	},
+})
+class ApplicationVersionAdd extends Vue {
+	@ApplicationStore.Action("addApplicationUpdate")
+	addApplicationUpdate!: Function;
+
+	@Emit()
+	private emitUpdateApplicationUpdateList() {
+		this.$emit("emitUpdateApplicationUpdateList");
+	}
+
+	form: any;
+	btnLoading: boolean = false;
+	planformOptions: Array<any> = [
+		{ label: "iOS", value: "ios" },
+		{ label: "Android", value: "android" },
+	];
+
+	debugOptions: Array<any> = [
+		{ label: "排除", value: 0 },
+		{ label: "包含", value: 1 },
+		{ label: "仅DEBUG", value: 2 },
+	];
+
+	updateModeOptions: Array<any> = [
+		{ label: "自动触发", value: 0 },
+		{ label: "客户端触发", value: 1 },
+	];
+
+	rebootOptions: Array<any> = [
+		{ label: "静默", value: 0 },
+		{ label: "自动重启", value: 1 },
+		{ label: "提示重启", value: 2 },
+	];
+
+	validOptions: Array<any> = [
+		{ label: "启用", value: 1 },
+		{ label: "暂停", value: 0 },
+		{ label: "撤回", value: 2 },
+	];
+	clearCacheOptions: Array<any> = [
+		{ label: "保留缓存", value: 0 },
+		{ label: "清除缓存", value: 1 },
+	];
+
+	rebootConfirmRebootOptions = [
+		{ label: "静默", value: 0 },
+		{ label: "重启", value: 1 },
+	];
+
+	async created() {}
+
+	protected mounted() {}
+
+	debugTipsMode = 0;
+	debugChange(e: any) {
+		this.debugTipsMode = e.target.value;
+	}
+	debugTips() {
+		console.log("debugTipsMode", this.debugTipsMode);
+
+		if (this.debugTipsMode === 0) {
+			return "DEBUG版本无法收到此更新包。";
+		}
+		if (this.debugTipsMode === 1) {
+			return "DEBUG版本可以收到此更新包。";
+		}
+		if (this.debugTipsMode === 2) {
+			return "只有DEBUG版本可以收到此更新包（一般用于测试）。";
+		}
+	}
+
+	validTipsMode = 1;
+	validChange(e: any) {
+		this.validTipsMode = e.target.value;
+	}
+	validTips() {
+		console.log("validTipsMode", this.validTipsMode);
+
+		if (this.validTipsMode === 0) {
+			return "暂停更新，用户收不到此更新，已更新的用户不影响。";
+		}
+		if (this.validTipsMode === 1) {
+			return "开启更新，所有用户都会收到此更新。";
+		}
+		if (this.validTipsMode === 2) {
+			return "回滚更新，用户收不到此更新，已更新的用户将撤回此更新。";
+		}
+	}
+
+	updateModeTipsMode = 0;
+	updateModeChange(e: any) {
+		this.updateModeTipsMode = e.target.value;
+	}
+	updateModeTips() {
+		console.log("updateModeTipsMode", this.updateModeTipsMode);
+
+		if (this.updateModeTipsMode === 0) {
+			return "自动触发，APP自动检测更新。";
+		}
+		if (this.updateModeTipsMode === 1) {
+			return "仅客户触发，通过执行方法 seui.checkUpdate() 触发更新。";
+		}
+	}
+
+	rebootTipsMode = 0;
+	rebootChange(e: any) {
+		this.rebootTipsMode = e.target.value;
+	}
+	rebootTips() {
+		console.log("rebootTipsMode", this.rebootTipsMode);
+
+		if (this.rebootTipsMode === 0) {
+			return "更新完后没有任何提示，更新内容一般在用户下次使用app时生效。";
+		}
+		if (this.rebootTipsMode === 1) {
+			return "更新完所有包后app自动重启，更新内容即刻生效。";
+		}
+		if (this.rebootTipsMode === 2) {
+			return "更新完此包后提示设置内容，根据用户自己选择。";
+		}
+	}
+
+	clearCacheTipsMode = 0;
+	clearCacheChange(e: any) {
+		this.clearCacheTipsMode = e.target.value;
+	}
+	clearCacheTips() {
+		console.log("clearCacherTipsMode", this.clearCacheTipsMode);
+
+		if (this.clearCacheTipsMode === 0) {
+			return "更新完后没有任何提示，更新内容一般在用户下次使用app时生效。";
+		}
+		if (this.clearCacheTipsMode === 1) {
+			return "更新完所有包后app自动重启，更新内容即刻生效。";
+		}
+		if (this.clearCacheTipsMode === 2) {
+			return "更新完此包后提示设置内容，根据用户自己选择。";
+		}
+	}
+
+	async handleSubmit(event: Event) {
+		event.preventDefault();
+		const {
+			form: { validateFields },
+		} = this;
+		validateFields(async (err: string, value: any) => {
+			if (!err) {
+				this.btnLoading = true;
+				const params: AddApplicationUpdateParams = {
+					title: value.title,
+					version: value.version,
+					platform: value.platform.toString(),
+					reboot: value.reboot,
+					rebootTitle: value.rebootTitle,
+					rebootMessage: value.rebootMessage,
+					rebootConfirmReboot: value.rebootConfirmReboot,
+					fileUrl: value.fileUrl,
+					valid: value.valid,
+					updateMode: value.updateMode,
+					clearCache: value.clearCache,
+					debug: value.debug,
+				};
+				await this.addApplicationUpdate(params);
+				this.emitUpdateApplicationUpdateList();
+				this.btnLoading = false;
+			}
+		});
+	}
+
+	protected render() {
+		const { getFieldDecorator } = this.form;
 		return (
-			<Card class={style.applicationItem} hoverable style="">
-				<div class={style.baseInfo} slot="cover">
-					<div class={style.title}>
-						<div class={style.cName}>{this.item.cName}</div>
-						<Tag>{this.item.name}</Tag>
-						<Tag>{this.renderAppType(this.item.appType)}</Tag>
-					</div>
-					<div class={style.desc}>{this.item.desc}</div>
-					<div class={style.ontherInfo}>
-						<div class={style.assignee}>
-							<Tooltip placement="top">
-								<div slot="title">负责人：</div>
-								<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-							</Tooltip>
-						</div>
-						<div class={style.version}>
-							<Tooltip placement="top">
-								<div slot="title">版本号</div>
-								<Tag>1.0 - 20200101</Tag>
-							</Tooltip>
-						</div>
-					</div>
-				</div>
-			</Card>
+			<Form onSubmit={this.handleSubmit}>
+				<Row gutter={8}>
+					<Col span="8">
+						<Form.Item label="热更新名称">
+							{getFieldDecorator("title", {
+								rules: [
+									{
+										required: true,
+										message: "热更新名称",
+									},
+								],
+							})(
+								<Input
+									type="text"
+									name="title"
+									placeholder="输入热更新名称"
+								></Input>
+							)}
+						</Form.Item>
+					</Col>
+					<Col span="8">
+						<Form.Item label="更新平台">
+							{getFieldDecorator("platform", {
+								initialValue: ["ios", "android"],
+								rules: [
+									{
+										required: true,
+										message: "工程名称不能为空",
+									},
+								],
+							})(
+								<Checkbox.Group
+									name="platform"
+									options={this.planformOptions}
+								></Checkbox.Group>
+							)}
+						</Form.Item>
+					</Col>
+					<Col span="8">
+						<Form.Item label="DEBUG版本" help={this.debugTips()}>
+							{getFieldDecorator("debug", {
+								initialValue: 0,
+								rules: [
+									{
+										required: true,
+										message: "请确定Debug类型",
+									},
+								],
+							})(
+								<Radio.Group
+									name="debug"
+									options={this.debugOptions}
+									on-change={this.debugChange}
+								></Radio.Group>
+							)}
+						</Form.Item>
+					</Col>
+				</Row>
+				<Form.Item
+					label="热更新版本"
+					help="iOS为Build编译版本号，安卓为versionCode应用版本号。（注：多个版本半角逗号分隔，如：12,13）"
+				>
+					{getFieldDecorator("version", {
+						rules: [
+							{
+								required: true,
+								message: "更新版本不能为空",
+							},
+						],
+					})(
+						<Select
+							show-search
+							allowClear
+							name="version"
+							placeholder="请选择更新版本（如果对多个版本进行更新请选择多个）"
+						>
+							<Select.Option key="native">前端-原生</Select.Option>
+						</Select>
+					)}
+				</Form.Item>
+				<Form.Item label="更新包URL">
+					{getFieldDecorator("fileUrl", {
+						rules: [
+							{
+								required: true,
+								message: "更新包URL",
+							},
+						],
+					})(
+						<Input
+							type="text"
+							name="fileUrl"
+							placeholder="输入更新包URL"
+						></Input>
+					)}
+				</Form.Item>
+				<Row gutter={8}>
+					<Col span="12">
+						<Form.Item label="启用状态" help={this.validTips()}>
+							{getFieldDecorator("valid", {
+								initialValue: 1,
+								rules: [
+									{
+										required: true,
+										message: "请选择更新模式",
+									},
+								],
+							})(
+								<Radio.Group
+									name="valid"
+									options={this.validOptions}
+									on-change={this.validChange}
+								></Radio.Group>
+							)}
+						</Form.Item>
+					</Col>
+					<Col span="12">
+						<Form.Item label="更新模式" help={this.updateModeTips()}>
+							{getFieldDecorator("updateMode", {
+								initialValue: 0,
+								rules: [
+									{
+										required: true,
+										message: "请选择更新模式",
+									},
+								],
+							})(
+								<Radio.Group
+									name="updateMode"
+									options={this.updateModeOptions}
+									on-change={this.updateModeChange}
+								></Radio.Group>
+							)}
+						</Form.Item>
+					</Col>
+				</Row>
+				<Row gutter={8}>
+					<Col span="12">
+						<Form.Item label="更新完成后-重启" help={this.rebootTips()}>
+							{getFieldDecorator("reboot", {
+								initialValue: 0,
+								rules: [
+									{
+										required: true,
+										message: "工程名称不能为空",
+									},
+								],
+							})(
+								<Radio.Group
+									name="reboot"
+									options={this.rebootOptions}
+									on-change={this.rebootChange}
+								></Radio.Group>
+							)}
+						</Form.Item>
+					</Col>
+					<Col span="12">
+						<Form.Item label="更新完成后-缓存处理" help={this.clearCacheTips()}>
+							{getFieldDecorator("clearCache", {
+								initialValue: 0,
+								rules: [
+									{
+										required: true,
+										message: "请确定clearCache类型",
+									},
+								],
+							})(
+								<Radio.Group
+									name="clearCache"
+									options={this.clearCacheOptions}
+									on-change={this.clearCacheChange}
+								></Radio.Group>
+							)}
+						</Form.Item>
+					</Col>
+				</Row>
+				{this.renderRebootConform()}
+				<Form.Item>
+					<Button
+						type="primary"
+						block
+						size="large"
+						html-type="submit"
+						loading={this.btnLoading}
+					>
+						新增
+					</Button>
+				</Form.Item>
+			</Form>
 		);
 	}
 
-	renderAppType(appType: string) {
-		if (appType === "native") {
-			return "原生";
+	renderVersion() {
+		return {
+			/* <Select.Option key="h5">前端-H5</Select.Option>
+		<Select.Option key="xcx">前端-小程序</Select.Option>
+		<Select.Option key="web">前端-后台</Select.Option>
+		<Select.Option key="java">后端-JAVA</Select.Option>
+		<Select.Option key="go">后端-Go</Select.Option>
+		<Select.Option key="ai">ai</Select.Option> */
+		};
+	}
+
+	renderRebootConform() {
+		const { getFieldDecorator } = this.form;
+		if (this.rebootTipsMode === 2) {
+			return (
+				<Row gutter={8}>
+					<Col span="16">
+						<Form.Item label="重启提示标题">
+							{getFieldDecorator("rebootTitle", {
+								rules: [
+									{
+										required: true,
+										message: "请选择更新模式",
+									},
+								],
+							})(
+								<Input
+									type="text"
+									name="rebootTitle"
+									placeholder="输入热更新重启提示标题"
+								></Input>
+							)}
+						</Form.Item>
+					</Col>
+					<Col span="16">
+						<Form.Item label="更新模式">
+							{getFieldDecorator("rebootMessage", {
+								rules: [
+									{
+										required: true,
+										message: "请选择更新模式",
+									},
+								],
+							})(
+								<Input.TextArea
+									type="text"
+									name="rebootMessage"
+									placeholder="输入热更新重启提示说明"
+								></Input.TextArea>
+							)}
+						</Form.Item>
+					</Col>
+					<Col span="16">
+						<Form.Item label="点击确定行为">
+							{getFieldDecorator("rebootConfirmReboot", {
+								initialValue: 0,
+								rules: [
+									{
+										required: true,
+										message: "点击确定行为",
+									},
+								],
+							})(
+								<Radio.Group
+									name="rebootConfirmReboot"
+									options={this.rebootConfirmRebootOptions}
+								></Radio.Group>
+							)}
+						</Form.Item>
+					</Col>
+				</Row>
+			);
 		}
 	}
 }
+export default Form.create({})(ApplicationVersionAdd);
--- src/components/Application/ApplicationVersionList/index.tsx
+++ src/components/Application/ApplicationVersionList/index.tsx
@@ -1,37 +1,228 @@
-import { Avatar, Card, Icon, Table, Tag, Tooltip } from "ant-design-vue";
+import {
+	Alert,
+	Avatar,
+	Button,
+	Card,
+	Col,
+	Divider,
+	Icon,
+	Modal,
+	Row,
+	Table,
+	Tabs,
+	Tag,
+	Tooltip,
+} from "ant-design-vue";
 import { Component, Vue, Prop, Emit } from "vue-property-decorator";
-import style from "./index.less";
-import { ApplicationMode } from "@/store/models/application/types";
-
+import styles from "./index.less";
+import {
+	ApplicationMode,
+	ApplicationUpdateMode,
+	ListApplicationUpdateParams,
+} from "@/store/models/application/types";
+import { namespace } from "vuex-class";
+const ApplicationStore = namespace("application");
+import ApplicationUpdateAdd from "@/components/Application/ApplicationUpdateAdd";
+import ApplicationUpdateEdit from "@/components/Application/ApplicationUpdateEdit";
 @Component
 export default class ApplicationVersionList extends Vue {
+	@ApplicationStore.Getter("currApplicationUpdateList")
+	currApplicationUpdateList!: Array<ApplicationUpdateMode>;
+	@ApplicationStore.Getter("currApplicationUpdateListCount")
+	currApplicationUpdateListCount!: number;
+	@ApplicationStore.Action("getApplicationUpdateList")
+	getApplicationUpdateList!: Function;
+	@ApplicationStore.Action("updateEditApplicationUpdate")
+	updateEditApplicationUpdate!: Function;
+	@ApplicationStore.Action("checkApplicationUpdate")
+	checkApplicationUpdate!: Function;
+
 	@Prop(Object) readonly item!: ApplicationMode;
 
-	versionListColumns: Array<any> = [
+	addUpdateModal: boolean = false;
+	editUpdateModal: boolean = false;
+	editorApplicationUpdateItem!: ApplicationUpdateMode;
+	updateListColumns: Array<any> = [
 		{
-			dataIndex: "name",
-			key: "name",
-			slots: { title: "customTitle" },
-			scopedSlots: { customRender: "name" },
+			dataIndex: "title",
+			key: "title",
+			title: "更新名称",
 		},
 		{
-			title: "Age",
-			dataIndex: "age",
-			key: "age",
+			title: "更新版本",
+			dataIndex: "version",
+			width: "300",
+			key: "version",
+			customRender: this.rednerVersion,
 		},
 		{
-			title: "Address",
-			dataIndex: "address",
-			key: "address",
+			title: "适合平台",
+			dataIndex: "platform",
+			key: "platform",
+			customRender: this.rednerPlatfrom,
+		},
+		{
+			title: "DEBUG版本",
+			dataIndex: "debug",
+			key: "debug",
+			customRender: this.rednerDebug,
+		},
+
+		{
+			title: "更新模式",
+			dataIndex: "updateMode",
+			key: "updateMode",
+			customRender: this.rednerUpdateMode,
+		},
+		{
+			title: "重启模式",
+			dataIndex: "reboot",
+			key: "reboot",
+			width: "200px",
+			customRender: this.rednerReboot,
+		},
+		{
+			title: "缓存处理",
+			dataIndex: "clearCache",
+			key: "clearCache",
+			customRender: this.rednerUpdateAction,
+		},
+		// {
+		// 	title: "已更新数量",
+		// 	dataIndex: "debug",
+		// 	key: "debug",
+		// },
+		{
+			title: "启用状态",
+			dataIndex: "valid",
+			key: "valid",
+			customRender: this.rednerValid,
+		},
+		// {
+		// 	title: "状态",
+		// 	dataIndex: "status",
+		// 	key: "status",
+		// 	customRender: this.rednerStatus,
+		// },
+		{
+			title: "操作",
+			customRender: this.renderAction,
 		},
 	];
-	versionListData: Array<any> = [];
+
+	currentPage: number = 1;
+	pageSize: number = 5;
+	async created() {
+		await this.__getApplicationUpdateList();
+	}
+
+	private addUpdateModalHandle() {
+		this.addUpdateModal = !this.addUpdateModal;
+	}
+	private editUpdateModalHandle() {
+		this.editUpdateModal = !this.editUpdateModal;
+	}
+
+	private async pageChange(e: any) {
+		console.log("e", e);
+		this.currentPage = e;
+		await this.__getApplicationUpdateList();
+	}
+
+	private async __getApplicationUpdateList() {
+		const params = {
+			pageIndex: this.currentPage,
+			pageSize: this.pageSize,
+		};
+		console.log("params", params);
+
+		await this.getApplicationUpdateList(params);
+	}
+
+	checkUpdateBtnLoading: boolean = false;
+	private async checkAppUpdate() {
+		this.checkUpdateBtnLoading = true;
+		const res = await this.checkApplicationUpdate();
+		this.checkUpdateBtnLoading = false;
+		if (res.uplists.length > 0) {
+			this.$success({
+				title: "发现热更新成功",
+				content: this.renderCheckAppUpdateTips(res.uplists[0]),
+			});
+		} else {
+			this.$error({
+				title: "当前没有可用热更新",
+			});
+		}
+	}
+
 	render() {
 		return (
-			<Table
-				columns={this.versionListColumns}
-				data-source={this.versionListData}
-			></Table>
+			<div class={styles.applicationUpdateList}>
+				<div class={styles.buttonGroups}>
+					<Button
+						slot="tabBarExtraContent"
+						type="primary"
+						on-click={this.addUpdateModalHandle}
+					>
+						添加热更新
+					</Button>
+					<Button
+						slot="tabBarExtraContent"
+						type="danger"
+						style={{ marginLeft: "8px" }}
+						loading={this.checkUpdateBtnLoading}
+						on-click={this.checkAppUpdate}
+					>
+						热更新测试
+					</Button>
+				</div>
+
+				<Table
+					rowKey="id"
+					columns={this.updateListColumns}
+					data-source={this.currApplicationUpdateList}
+					pagination={{
+						pageSize: this.pageSize,
+						current: this.currentPage,
+						total: this.currApplicationUpdateListCount,
+						onChange: this.pageChange,
+						showTotal: (total: number) => `总共${total}条`,
+					}}
+				></Table>
+				<Modal
+					title="新增热更新"
+					visible={this.addUpdateModal}
+					width={1000}
+					dialog-style={{ top: "20px" }}
+					on-cancel={this.addUpdateModalHandle}
+					maskClosable={false}
+					footer={null}
+				>
+					<ApplicationUpdateAdd
+						on-emitUpdateApplicationUpdateList={async () => {
+							this.addUpdateModal = !this.addUpdateModal;
+							await this.__getApplicationUpdateList();
+						}}
+					/>
+				</Modal>
+				<Modal
+					title="编辑热更新"
+					visible={this.editUpdateModal}
+					width={1000}
+					dialog-style={{ top: "20px" }}
+					on-cancel={this.editUpdateModalHandle}
+					maskClosable={false}
+					footer={null}
+				>
+					<ApplicationUpdateEdit
+						on-emitUpdateApplicationUpdateList={async () => {
+							this.editUpdateModal = !this.editUpdateModal;
+							await this.__getApplicationUpdateList();
+						}}
+					/>
+				</Modal>
+			</div>
 		);
 	}
 
@@ -39,5 +230,162 @@ export default class ApplicationVersionList extends Vue {
 		if (appType === "native") {
 			return "原生";
 		}
+	}
+
+	private rednerVersion(text: any, record: any, index: number) {
+		const verson: Array<string> = record.version.split(",");
+		return verson.map((item) => {
+			return <Tag color="blue">{item}</Tag>;
+		});
+	}
+
+	private rednerPlatfrom(text: any, record: any, index: number) {
+		let platform = record.platform.split(",");
+		if (platform.length > 1) {
+			return (
+				<div>
+					<Icon type="android" theme="filled" style={{ color: "#2def0a" }} />
+					<Icon type="apple" theme="filled" />
+				</div>
+			);
+		} else {
+			if (platform[0] === "ios") {
+				return <Icon type="apple" />;
+			}
+			if (platform[0] === "android") {
+				return <Icon type="android" style={{ color: "#2def0a" }} />;
+			}
+		}
+	}
+	private rednerDebug(text: any, record: any, index: number) {
+		if (record.debug === "0") {
+			return <Tag color="blue">排除Debug</Tag>;
+		}
+		if (record.debug === "1") {
+			return <Tag color="orange">包含Debug</Tag>;
+		}
+		if (record.debug === "2") {
+			return <Tag color="red">仅限Debug</Tag>;
+		}
+	}
+
+	private rednerReboot(text: any, record: any, index: number) {
+		if (record.reboot === "0") {
+			return <Tag color="blue">静默</Tag>;
+		}
+		if (record.reboot === "1") {
+			return <Tag color="orange">自动重启</Tag>;
+		}
+		if (record.reboot === "2") {
+			return (
+				<Row>
+					<Tag color="red">提示重启</Tag>
+					<Card style={{ marginTop: "8px" }} title={record.rebootTitle}>
+						<div>{record.rebootMessage}</div>
+						<div slot="actions">
+							确认按钮行为:
+							{record.rebootConfirmReboot === "0" ? (
+								<Tag color="blue">静默</Tag>
+							) : (
+								<Tag color="orange">自动重启</Tag>
+							)}
+						</div>
+					</Card>
+				</Row>
+			);
+		}
+	}
+
+	private rednerUpdateMode(text: any, record: any, index: number) {
+		if (record.updateMode === "0") {
+			return <Tag color="blue">自动触发</Tag>;
+		}
+		if (record.updateMode === "1") {
+			return <Tag color="orange">客户端触发</Tag>;
+		}
+	}
+
+	private rednerUpdateAction(text: any, record: any, index: number) {
+		if (record.clearCache === "0") {
+			return <Tag color="blue">保留缓存</Tag>;
+		}
+		if (record.clearCache === "1") {
+			return <Tag color="red">清除缓存</Tag>;
+		}
+	}
+
+	private rednerValid(text: any, record: any, index: number) {
+		if (record.valid === "1") {
+			return <Tag color="blue">启用</Tag>;
+		}
+		if (record.valid === "0") {
+			return <Tag color="orange">暂停</Tag>;
+		}
+		if (record.valid === "2") {
+			return <Tag color="red">撤回</Tag>;
+		}
+	}
+
+	private rednerStatus(text: any, record: any, index: number) {
+		if (record.status === "0") {
+			return <Tag color="blue">正常</Tag>;
+		}
+		if (record.status === "1") {
+			return <Tag color="red">删除</Tag>;
+		}
+	}
+
+	private renderAction(text: any, record: any, index: number) {
+		return (
+			<div class={styles.actionButton}>
+				<Button
+					type="primary"
+					size="small"
+					on-click={() => {
+						this.updateEditApplicationUpdate(record);
+						this.editUpdateModalHandle();
+					}}
+				>
+					编辑
+				</Button>
+				<Button type="danger" size="small">
+					删除
+				</Button>
+			</div>
+		);
+	}
+
+	private renderCheckAppUpdateTips(updateData: any) {
+		return (
+			<Alert type="info">
+				<div slot="message">
+					<Row gutter={8}>
+						<Col span="8">重启类型:</Col>
+						<Col span="16">
+							{this.rednerReboot(
+								null,
+								{
+									reboot: updateData.reboot,
+									rebootTitle: updateData.reboot_info.title,
+									rebootMessage: updateData.reboot_info.message,
+									rebootConfirmReboot: updateData.reboot_info.confirm_reboot,
+								},
+								0
+							)}
+						</Col>
+					</Row>
+					<Row gutter={8} style={{ marginTop: "8px" }}>
+						<Col span="8">是否清除缓存:</Col>
+						<Col span="16">
+							{this.rednerUpdateAction(
+								null,
+								{ clearCache: updateData.clear_cache },
+								0
+							)}
+						</Col>
+					</Row>
+				</div>
+			</Alert>
+		);
 	}
 }
